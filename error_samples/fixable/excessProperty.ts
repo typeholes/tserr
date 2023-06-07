4239c457122ddb94a81d454f95b0dbe9dc27d4e8
typeholes <typeholes@protonmail.net>
2023-06-07 13:17:23 -0400

    overload example for excess property checks

--- error_samples/fixable/excessProperty.ts
+++ error_samples/fixable/excessProperty.ts
@@ -2,3 +2,11 @@ const t: { a: 1 } = { a: 1, b: 2 };
 
 // not an excess property check so the fix should not show
 const n: number = 's';
+
+function over(o: { a: 1 }): number;
+function over(o: { b: 2 }): number;
+function over(o: { a?: 1; b?: 2 }) {
+  return 1;
+}
+
+over({a:1, c: 2})
