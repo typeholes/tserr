c98763bbf504ccf247921c48d390470f59c876d0
typeholes <typeholes@protonmail.net>
2023-06-12 10:40:55 -0400

    slightly less naive parrser

--- packages/tserr-server/src/parseError.ts
+++ packages/tserr-server/src/parseError.ts
@@ -1,10 +1,50 @@
-import { ParsedError } from "@typeholes/tserr-common";
+import { ParsedError } from '@typeholes/tserr-common';
 
 export function parseError(err: string): ParsedError {
-  const parts = err.split("'").map((part) => part.trim());
+  const rawParts = err.split("'");
+
+  const parts: string[] = [''];
+
+  let i = 0;
+  let state: 'raw' | 'quote' | 'double' = 'raw';
+  err.split('').forEach((c) => {
+    if (c === undefined) {
+      return;
+    }
+    if (state === 'raw') {
+      if (c === "'") {
+        state = 'quote';
+        i++;
+        parts[i] = '';
+      } else {
+        parts[i] += c;
+      }
+    } else if (state === 'quote') {
+      if (c === '"') {
+        state = 'double';
+        parts[i] += c;
+      } else if (c === "'") {
+        state = 'raw';
+        parts[++i] = '';
+      } else {
+        parts[i] += c;
+      }
+    } else if (state === 'double') {
+      if (c === '"') {
+        state = 'quote';
+      }
+      parts[i] += c;
+    }
+  });
+
+  for (let i=0;i<parts.length;i++) { parts[i] = parts[i].trim()}
+
+  if (parts[parts.length - 1] === '.') {
+    parts.length--;
+  }
 
   if (
-    (parts.length === 5 &&
+    (parts.length === 4 &&
       parts[0] === 'Type' &&
       parts[2] === 'is not assignable to type') ||
     parts[2] === 'is not assignable to parameter of type'
@@ -15,7 +55,7 @@ export function parseError(err: string): ParsedError {
       to: parts[3],
     };
   } else if (
-    parts.length === 5 &&
+    parts.length === 4 &&
     parts[2] === 'implicitly has type' &&
     parts[4] ===
       'because it does not have a type annotation and is referenced directly or indirectly in its own initializer.'
@@ -39,14 +79,14 @@ export function parseError(err: string): ParsedError {
     const length = Number.parseInt(overloadParts[3]) ?? -1;
     return { type: 'overloadPiece', idx, length, signature: parts[1] };
   } else if (
-    parts.length === 5 &&
+    parts.length === 4 &&
     parts[0] === 'Object literal may only specify known properties, and'
   ) {
     return {
-      type: "excessProperty",
+      type: 'excessProperty',
       key: parts[1],
       to: parts[3],
-    }
+    };
   } else {
     return {
       type: 'unknownError',
