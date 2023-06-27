f443b6e6f8239e4c133a3ba70dd82be00da9d856
typeholes <typeholes@protonmail.net>
2023-06-27 14:35:41 -0400

    chore: show ignored codes in config

--- packages/tserr-vue/src/app/components/ConfigView.vue
+++ packages/tserr-vue/src/app/components/ConfigView.vue
@@ -48,15 +48,18 @@ const dummy = ref(false);
   <div>
     <!-- foo: {{ JSON.stringify(appState.configs, null, 2) }} -->
     <!-- foo: {{ JSON.stringify(asTree(), null, 2) }} -->
-    <!-- <template v-for="item of tree" :key="item.id">
-      <ConfigTree :item="item" />
-    </template> -->
     <q-tree dense :nodes="asTree()" node-key="label">
       <template #default-body="props">
         {{ props.node.title }}
         <template v-for="ts of props.node.config.tsconfig" :key="ts">
           <q-checkbox :label="ts as string" :model-value="dummy" />
         </template>
+        <div class="row q-gutter-xs" v-if="props.node.config?.config?.ignoreErrCodes?.length>0">
+          <div class="col-auto"> ignore:   </div>
+        <template v-for="code of props.node.config?.config?.ignoreErrCodes" :key="code">
+          <div class="col-auto"> {{ code }}  </div>
+        </template>
+      </div>
       </template>
     </q-tree>
   </div>
