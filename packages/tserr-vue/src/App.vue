9af25c4c22aae3925d2a2dda989b0b52896509fd
typeholes <typeholes@protonmail.net>
2023-06-28 19:21:18 -0400

    fix: removed UI footer

--- packages/tserr-vue/src/App.vue
+++ packages/tserr-vue/src/App.vue
@@ -64,7 +64,7 @@ function refresh() {
       <AppState />
     </q-page-container>
 
-    <q-footer elevated class="bg-grey-8 text-white">
+    <!-- <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -73,7 +73,7 @@ function refresh() {
           <div>TsErr Problems View</div>
         </q-toolbar-title>
       </q-toolbar>
-    </q-footer>
+    </q-footer> -->
   </q-layout>
 </template>
 
